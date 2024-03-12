50f1fa2a9fa9f8aca4718db52dd0827a886a06b6
Lakshman <lakshman.automation@gmail.com>
2024-03-12 10:43:18 +0530

    created api for articles index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "application#index"
   get "home", to: "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show]
+  resources :articles#, only: [:show]
 end
